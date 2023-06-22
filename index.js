ffb949704d9441f52ce6eead14621a0a34f5e713
whiplade <nmawira13@gmail.com>
2023-06-22 11:49:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const element = document.querySelector('#main');
+element.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "Whiplade is the champion!"
+
